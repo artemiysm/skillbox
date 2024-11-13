82444954f7c57e59e0e741adb61e594ae9206d41
artemiysm <95012548+artemiysm@users.noreply.github.com>
2024-11-13 09:29:54 +0700

    Update main.js

--- 09/main.js
+++ 09/main.js
@@ -7,7 +7,7 @@ const generateCardNumbers = (pairsCount = 8) => {
     return numbers;
 };
 
-// Функция для перемешивания массива чисел
+// перемешивание массива чисел
 const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
